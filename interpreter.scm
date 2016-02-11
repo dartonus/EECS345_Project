93fa1585550605f1579b23891006389fc7c9f7ba
dartonus <callum.grant@verizon.net>
2016-02-10 23:39:10 -0500

    Added preliminary version of "while" and evaluate
    While is currently nonfunctional, pending an alteration of the
    evaluation check. Evaluate was tested in a slightly modified form (one
    that made no use of State or mvalue) and was found to function as
    expected.

--- interpreter.scm
+++ interpreter.scm
@@ -33,3 +33,37 @@
 
 
 |#
+
+; state - the state. line - the line that the while occurs in (i.e. the segment enclosed by parentheses, starting with "while")
+; (cadr line) - gets the element that is second in the provided line, which should be the clause for the while loop.
+; (caddr line) gets the third element in the provided line, which should be the procedure for the while loop.
+#|(define whilehandler
+  (lambda (state line)
+    (cond
+      ((!eq (evaluate (cadr line)) #t) (begin (perform (caddr line)) (whilehandler state line))))))|#
+;assumed functions: "evaluate" - checks a logical equation. "perform" - performs the action of the segment (e.g., defines a variable if an "(= x 10)" segment.)
+(define evaluate
+  (lambda (state line)
+    (cond
+      ((number? line) line)
+      ((eq? '= (logicsymbol line)) (eq? (mvalue (operand1 line)) (mvalue (operand2 line))))
+      ((eq? '!= (logicsymbol line)) (if (eq? (mvalue (operand1 line)) (mvalue (operand2 line)))
+                                           #f
+                                           #t))
+      ((eq? '> (logicsymbol line)) (> (mvalue (operand1 line)) (mvalue (operand2 line))))
+      ((eq? '>= (logicsymbol line)) (>= (mvalue (operand1 line)) (mvalue (operand2 line))))
+      ((eq? '< (logicsymbol line)) (< (mvalue (operand1 line)) (mvalue (operand2 line))))
+      ((eq? '<= (logicsymbol line)) (<= (mvalue (operand1 line)) (mvalue (operand2 line))))
+      (else (error 'unknown "unknown")))))
+
+(define logicsymbol
+  (lambda (input)
+    (car input)))
+
+(define operand1
+  (lambda (input)
+    (cadr input)))
+
+(define operand2
+  (lambda (input)
+    (caddr input)))
