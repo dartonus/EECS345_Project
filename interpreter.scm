c30598dea80806d472bf643499948fd12577c2b4
dartonus <callum.grant@verizon.net>
2016-02-11 13:21:54 -0500

    Minor tweaks to While stuff
    fixed the logic check to use not, corrected method calls to "m_value"

--- interpreter.scm
+++ interpreter.scm
@@ -121,10 +121,13 @@
 ; state - the state. line - the line that the while occurs in (i.e. the segment enclosed by parentheses, starting with "while")
 ; (cadr line) - gets the element that is second in the provided line, which should be the clause for the while loop.
 ; (caddr line) gets the third element in the provided line, which should be the procedure for the while loop.
-#|(define whilehandler
+
+#|
+(define whilehandler
   (lambda (state line)
     (cond
-      ((!eq (evaluate (cadr line)) #t) (begin (perform (caddr line)) (whilehandler state line))))))|#
+      ((not (eq (evaluate (cadr line)))) (begin (perform (caddr line)) (whilehandler state line))))))
+|#
 ;assumed functions: "evaluate" - checks a logical equation. "perform" - performs the action of the segment (e.g., defines a variable if an "(= x 10)" segment.)
 
 
@@ -132,14 +135,12 @@
   (lambda (state line)
     (cond
       ((number? line) line)
-      ((eq? '= (logicsymbol line)) (eq? (mvalue (operand1 line)) (mvalue (operand2 line))))
-      ((eq? '!= (logicsymbol line)) (if (eq? (mvalue (operand1 line)) (mvalue (operand2 line)))
-                                           #f
-                                           #t))
-      ((eq? '> (logicsymbol line)) (> (mvalue (operand1 line)) (mvalue (operand2 line))))
-      ((eq? '>= (logicsymbol line)) (>= (mvalue (operand1 line)) (mvalue (operand2 line))))
-      ((eq? '< (logicsymbol line)) (< (mvalue (operand1 line)) (mvalue (operand2 line))))
-      ((eq? '<= (logicsymbol line)) (<= (mvalue (operand1 line)) (mvalue (operand2 line))))
+      ((eq? '= (logicsymbol line)) (eq? (m_value (operand1 line)) (m_value (operand2 line))))
+      ((eq? '!= (logicsymbol line)) (not (eq? (m_value (operand1 line)) (m_value (operand2 line)))))
+      ((eq? '> (logicsymbol line)) (> (m_value (operand1 line)) (m_value (operand2 line))))
+      ((eq? '>= (logicsymbol line)) (>= (m_value (operand1 line)) (m_value (operand2 line))))
+      ((eq? '< (logicsymbol line)) (< (m_value (operand1 line)) (m_value (operand2 line))))
+      ((eq? '<= (logicsymbol line)) (<= (m_value (operand1 line)) (m_value (operand2 line))))
       (else (error 'unknown "unknown")))))
 
 (define logicsymbol
