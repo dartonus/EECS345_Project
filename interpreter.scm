f5e133d6b99157103b5e0b2e9f9e134d76a94efa
Jiawei Wu <abc1929@Gmail.com>
2016-02-10 19:42:54 -0500

    trying to get simple.txt parsed correctly

--- interpreter.scm
+++ interpreter.scm
@@ -1,1 +1,35 @@
+#lang racket
 (load "simpleParser.scm")
+(load "lex.scm")
+
+#|
+
+;basic expression evaluator used in class
+
+(define returnx
+  (lambda (returnx)
+    (cond
+      ((number? returnx) returnx)
+      ((eq? '+ (operator returnx)) (+ (mvalue (operand1 returnx)) (mvalue (operand2 returnx))))
+      ((eq? '- (operator returnx)) (- (mvalue (operand1 returnx)) (mvalue (operand2 returnx))))
+      ((eq? '* (operator returnx)) (* (mvalue (operand1 returnx)) (mvalue (operand2 returnx))))
+      ((eq? '/ (operator returnx)) (quotient (mvalue (operand1 returnx)) (mvalue (operand2 returnx))))
+      ((eq? '% (operator returnx)) (remainder (mvalue (operand1 returnx)) (mvalue (operand2 returnx))))
+      (else (error 'unknown "unknown")))))
+
+;infix parser
+
+(define operator
+  (lambda (input)
+    (cadr input)))
+
+(define operand1
+  (lambda (input)
+    (car input)))
+
+(define operand2
+  (lambda (input)
+    (caddr input)))
+
+
+|#
