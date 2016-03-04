eff7bc44631e681ca07503b60574b4acb14635d7
Jack Donnelly <deltadonnelly@gmail.com>
2016-03-04 15:23:11 -0500

    as
    asasas

--- interpreter.scm
+++ interpreter.scm
@@ -2,20 +2,25 @@
 (load "simpleParser.scm")
 (load "lex.scm")
 
-;write a procedural interpreter here that go through the parsed list (test.txt) using our functions
-;or I got the idea wrong?
+;Team: Callum Grant (chg33), Jiawei Wu (jxw585), John Donnelly (jed126)
 
-
-;this might be something we want?
-;might need set! for the state s somewhere along the line
 ;-------------------------start-----------------------------
 (define interpret
+  (lambda (filename)
+    (interpreter (parser filename) '(()()))
+    )
+  )
+
+;Interpretation method, runs through each sublist within the list of lists returned by parser, effectively going through the original text line-by-line.
+
+(define interpreter
   (lambda (parsed s)
     (begin
       ;intializing a state variable 
       (if (empty? (cdr parsed)) 
         (perform (car parsed) s)
-        (begin (perform (car parsed) s) (interpret (cdr parsed) (perform (car parsed) s))) ;perform (car list) and go interpret (cdr list)
+       ;(begin (perform (car parsed) s) (interpreter (cdr parsed) (perform (car parsed) s))) ;perform (car list) and go interpreter (cdr list)
+       (interpreter (cdr parsed) (perform (car parsed) s))
       )
     )
   )
@@ -23,7 +28,12 @@
 ;-------------------------end-----------------------------
 
 
-
+(define layered
+  (lambda (state)
+    (if (empty? (car state))
+      #f
+    (list? (caar state))))
+)
 
 
 ;helpers for state operations
@@ -45,7 +55,10 @@
 
 (define m_insert
   (lambda (name value s)
-    (append (list (append (car s) (list name))) (list (append (cadr s) (list (m_value value s)))))
+    (if (layered s)
+      (cons (m_insert name value (car s)) (cdr s))
+      (append (list (append (car s) (list name))) (list (append (cadr s) (list (box (m_value value s))))))
+      )
   )
 )
 
@@ -59,65 +72,89 @@
 
 (define m_remove
   (lambda (name s)
-    (cond
-      ((null? s) s)
-      ((empty? (car s)) '())
-      ((eq? name (caar s)) (remain s))
-      (else (append (list (append (list (car (extract s))) (car (m_remove name (remain s))))) (list (flatten (append (cdr (extract s)) (cdr (m_remove name (remain s))))))))
-    )
+    (if (layered s)
+      (cons (m_remove name (car s)) (cdr s))
+      (cond
+        ((null? s) s)
+        ((empty? (car s)) '())
+        ((eq? name (caar s)) (remain s))
+        (else (append (list (append (list (car (extract s))) (car (m_remove name (remain s))))) (list (flatten (append (cdr (extract s)) (cdr (m_remove name (remain s))))))))
+      ))
   )
 )
 
-
+;Finds the value of a variable of a given name within our state.
 (define lookup
     (lambda (name s)
-      (cond 
-        ((null? s) "undefined")
-        ;((void? s) "undefined")
-        ((empty? (car s)) "undefined")
-        ((eq? name (caar s)) (caadr s))
-        (else (lookup name (remain s)))
-    )
+      (if (not (layered s))
+        (cond 
+          ((null? s) "undefined")
+          ;((void? s) "undefined")
+          ((empty? (car s)) "undefined")
+          ((eq? name (caar s)) (unbox (caadr s)))
+          (else (lookup name (remain s)))
+      )
+
+        (if (eq? "undefined" (lookup name (car s)))
+          (lookup name (cdr s))
+          (lookup name (car s))))
+  )
+)
+
+(define lookupbox
+    (lambda (name s)
+      (if (not (layered s))
+        (cond 
+          ((null? s) "undefined")
+          ;((void? s) "undefined")
+          ((empty? (car s)) "undefined")
+          ((eq? name (caar s)) (caadr s))
+          (else (lookupbox name (remain s)))
+      )
+
+        (if (eq? "undefined" (lookupbox name (car s)))
+          (lookupbox name (cdr s))
+          (lookupbox name (car s))))
   )
 )
 
 
-;I don't think we need m_name functionally, since the x  in x = 5 is parsed as 'x already
-;Maybe declaration needs it?
 
-#|
-(define m_name
-  (lambda name
-    (list name))
-)
-|#
+; Assigns a value to a variable, and modifies our state accordingly.
 
 
-; expressions like '(= x 5) inplementation (assignment)
+;almost working, need to use set-box! to change state
+;basic structure laid down
 
 (define m_state
   (lambda (expression s)
-    (if (eq? (lookup (cadr expression) s) "undefined")
-    "error"
-      (m_insert (cadr expression) (m_value (caddr expression) s) (m_remove (cadr expression) s))
-    )
+        (if (eq? (lookup (cadr expression) s) "undefined")
+          (error 'error "Use before declaration")
+          (if (layered s)
+            (begin (set-box! (lookupbox (cadr expression) s) (m_value (caddr expression) (car s))) s)
+            (begin (set-box! (lookupbox (cadr expression) s) (m_value (caddr expression) s)) s)
+          )
+        )
+      )
   )
-)
 
-;skeleton for declaration parse, working on it
-
+; Declares a given variable
 (define m_declare
   (lambda (expression s)
-    (if (eq? (car expression) 'var)
-      (if (not (null? (cddr expression)))
-        (def_with_value (cadr expression) (m_value (caddr expression) s) s) ;parse "var x (things)" (what about declare booleans?)
-        (def_null (cadr expression) s);else parse "var x"
+    (if (layered s)
+      (cons (m_declare expression (car s)) (cdr s))
+        (if (eq? (car expression) 'var)
+          (if (not (null? (cddr expression)))
+            (def_with_value (cadr expression) (m_value (caddr expression) s) s) ;parse "var x (things)" (what about declare booleans?)
+            (def_null (cadr expression) s);else parse "var x"
+          )
+          "not valid declare"
+        )
       )
-      "not valid declare"
-    )
   )
 )
 
+; If the defined variable is defined with a value, this helper assigns it a value.
 (define def_with_value
   (lambda (var expression s) 
     (cond ((not (eq? (lookup var s) "undefined")) (begin (error 'error "no re-declaring") (s))) ;no re-declaring
@@ -125,6 +162,7 @@
   )
 )
 
+; If the defined variable has no value, this helper sets it up with a null value.
 (define def_null
   (lambda (var s)
     (cond ((not (eq? (lookup var s) "undefined")) (begin (error 'error "no re-declaring") (s))) ;no re-declaring
@@ -133,57 +171,59 @@
 )
 
 
-;basic expression evaluator used in class
-
-;doesn't have hierachy yet
+;Modified version of the basic expression evaluator from class.
 (define m_value
   (lambda (expression s)
-    (cond
-      ((eq? 'null expression) 'null) ;for declaration of a new variable
-      ((symbol? expression) (lookup expression s))
-      ((number? expression) expression)
-      ((eq? '+ (operator expression)) (+ (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
-      ((eq? '- (operator expression)) (- (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
-      ((eq? '* (operator expression)) (* (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
-      ((eq? '/ (operator expression)) (quotient (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
-      ((eq? '% (operator expression)) (remainder (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
-      (else (error 'unknown "unknown")))))
+    (if (layered s) (m_value expression (car s))
+      (cond
+        ((eq? 'null expression) 'null) ;for declaration of a new variable
+        ((eq? 'true expression) #t)
+        ((eq? 'false expression) #f)
+        ((symbol? expression) (lookup expression s))
+        ((number? expression) expression)
+        ((boolean? expression) expression)
+        ((eq? '= (operator expression)) (m_value (m_value (operand2 expression) s) (m_state expression s)))
+        ((eq? '+ (operator expression)) (+ (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '- (operator expression)) (if (null? (cddr expression))
+                                            (- 0 (m_value (operand1 expression) s))
+                                            (- (m_value (operand1 expression) s) (m_value (operand2 expression) s))))
+        ((eq? '* (operator expression)) (* (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '/ (operator expression)) (quotient (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '% (operator expression)) (remainder (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '== (operator expression)) (eq? (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '!= (operator expression)) (not (eq? (m_value (operand1 expression) s) (m_value (operand2 expression) s))))
+        ((eq? '> (operator expression)) (> (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '>= (operator expression)) (>= (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '< (operator expression)) (< (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '<= (operator expression)) (<= (m_value (operand1 expression) s) (m_value (operand2 expression) s)))
+        ((eq? '&& (operator expression)) (and (evaluate (operand1 expression) s) (evaluate (operand2 expression) s)))
+        ((eq? '|| (operator expression)) (or (evaluate (operand1 expression) s) (evaluate (operand2 expression) s)))
+        (else (error 'unknown "unknown"))))
+
+      ))
 
 ;prefix parser
-
 (define operator
   (lambda (input)
     (car input)))
 
-#|
-(define operand1
-  (lambda (input)
-    (cadr input)))
 
-(define operand2
-  (lambda (input)
-    (caddr input)))
-|#
-
-
+; Whilehandler addresses situations of the while loop. If the condition is not met, it performs the clause and then recurs.
 ; state - the state. line - the line that the while occurs in (i.e. the segment enclosed by parentheses, starting with "while")
 ; (cadr line) - gets the element that is second in the provided line, which should be the clause for the while loop.
 ; (caddr line) gets the third element in the provided line, which should be the procedure for the while loop.
-
-
 (define whilehandler
   (lambda (line state)
     (if (not (evaluate (cadr line) state))
        state
        (whilehandler line (perform (caddr line) state)))))
 
-;assumed functions: "evaluate" - checks a logical equation. "perform" - performs the action of the segment (e.g., defines a variable if an "(= x 10)" segment.)
-
-
+; Evaluate is a shortened version of the earlier expression evaluator, intended for use with logical clauses such as those used by If and While.
 (define evaluate
   (lambda (line state)
     (cond
       ((number? line) line)
+      ((symbol? line) (lookup line state))
       ((eq? '== (logicsymbol line)) (eq? (m_value (operand1 line) state) (m_value (operand2 line) state)))
       ((eq? '!= (logicsymbol line)) (not (eq? (m_value (operand1 line) state) (m_value (operand2 line) state))))
       ((eq? '> (logicsymbol line)) (> (m_value (operand1 line) state) (m_value (operand2 line) state)))
@@ -192,8 +232,10 @@
       ((eq? '<= (logicsymbol line)) (<= (m_value (operand1 line) state) (m_value (operand2 line) state)))
       ((eq? '&& (logicsymbol line)) (and (evaluate (operand1 line) state) (evaluate (operand2 line) state)))
       ((eq? '|| (logicsymbol line)) (or (evaluate (operand1 line) state) (evaluate (operand2 line) state)))
+      ((eq? '! (logicsymbol line)) (not (m_value (operand1 line) state)))
       (else (error 'unknown "unknown")))))
 
+;Definitions to avoid having to repeatedly type "car", "cadr", and "caddr" for Evaluate, and make it clear what is being grabbed in each case.
 (define logicsymbol
   (lambda (input)
     (car input)))
@@ -206,36 +248,103 @@
   (lambda (input)
     (caddr input)))
 
+;Performs the task of a given line, by calling the method that pertains to the line's opening.
 (define perform
   (lambda (line state)
-    (cond
-      ((eq? (car line) 'var) (m_declare line state))
-      ((eq? (car line) '=) (m_state line state))
-      ((eq? (car line) 'return) (display (m_value (cadr line) state)))
-      ((eq? (car line) 'if) (ifhandler line state))
-      ((eq? (car line) 'while) (whilehandler line state)))))
+    (call/cc (lambda (break)
+      (cond
+        ((eq? (car line) 'var) (m_declare line state))
+        ((eq? (car line) '=) (m_state line state))
+        ;return needs revamp (immediate break)
+        ((eq? (car line) 'return) (cond
+                                    ((eq? (cadr line) 'state) state)
+                                    ;above line is for debugging
+                                    ((eq? (m_value (cadr line) state) #t) (display 'true))
+                                    ((eq? (m_value (cadr line) state) #f) (display 'false))
+                                    (else (display (m_value (cadr line) state)))
+                                    ))
+        ((eq? (car line) 'if) (ifhandler line state))
+        ((eq? (car line) 'while) (whilehandler line state)) 
+        ((eq? (car line) 'begin) 
+          ;(call/cc (lambda (return)
+           (cdr (blockhandler (cdr line) (cons state state) #f))) ;block handler
+        
+
+        ((eq? (car line) 'continue) state) ;dummy
+        ((eq? (car line) 'break) 
+            (cond 
+              ((not (layered state)) (break (error 'error "Break must be inside a block"))) 
+              ;if break is encountered, throw away current layer immediately, 
+              ;but we need to skip the remaining lines in the braces of (begin (xx) (break) (xxx))
+              ;how to implement call/cc to do exactly that?
+              (else (break (breakhandler line state))) ;dummy
+            )
+        )))
+      )))
 
 
 
-;work on if
-;assumed functions: "evaluate" - checks a logical equation. "perform" - performs the action of the segment
-;If the condition is true the first statement needs to be evaluated.
-;If it's false then the else must be evaluated
-;However the fact that its optional may be an issue
-
+;Ifhandler - does its clause if the condition is met. If condition is not met, AND an else clause is present, performs the else clause.
 (define ifhandler
   (lambda (line state)
     (cond
       ((eq? (evaluate (cadr line) state) #t) (perform (caddr line) state))
+<<<<<<< HEAD
       ((null? (itemn line 3)) state)
       (else (perform (itemn line 3) state)))))
+=======
+      ((null? (itemn line 4)) state)
+      (else (perform (itemn line 4) state)))))
+>>>>>>> origin/master
 
 
 ;find the nth item in a list
  (define itemn
    (lambda (l n) 
       (cond
+        ((null? l) '())
         ((eq? n 0) '())
         ((eq? n 1) (car l))
         (else (itemn (cdr l) (- n 1))))))
+
+
+
+
+(define blockhandler
+	(lambda (line s break)
+    (call/cc (lambda (breakout)
+        (if break
+            (breakout s)       
+            (if (empty? (cdr line)) 
+              (perform (car line) s)
+              (blockhandler (cdr line) (perform (car line) s) break)))
+      )
+    )
+  )
+)
         
+(define gotohandler (lambda (v) v))
+
+
+(define breakhandler
+  (lambda (line s)
+    (blockhandler line s #t)
+  )
+)
+
+
+(define continuehandler 
+  (lambda (line s)
+    (call/cc
+        (s)
+      )
+  )
+)
+
+
+
+(define trycatchhandler (lambda (v) v))
+
+
+
+(define throwhandler (lambda (v) v))
